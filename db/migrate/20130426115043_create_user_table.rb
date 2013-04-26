e64b617da26aa6b9880e21efcb84960a40175fcd
Apprentice <apprentice@devbootcamp.com>
2013-04-26 12:02:13 -0700

    user model and migration

---
+++ db/migrate/20130426115043_create_user_table.rb
@@ -0,0 +1,9 @@
+class CreateUserTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, :null => false
+      t.string :email, :null => false
+      t.string :password, :null => false
+    end
+  end
+end
