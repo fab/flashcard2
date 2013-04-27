eb8e751f83217db386c453359b4d4cdbe1b66eca
Fab Mackojc <fab.mackojc@gmail.com>
2013-04-26 19:01:01 -0700

    users now also have incorrect answers for each card

--- app/helpers/index.rb
+++ app/helpers/index.rb
@@ -2,4 +2,9 @@ helpers do
   def partial(template, locals = {})
     erb(template, :layout => false, :locals => locals)
   end
+
+  def three_wrong_answers(card)
+    three_cards = @deck.cards.where('id != ?', card.id).sample(3)
+    three_cards.map { |card| card.answer }
+  end
 end
