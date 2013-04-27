1dfa51a9a33647399ba534c8ac1bd033032a7ff7
sidazhang <fab.mackojc@gmail.com>
2013-04-26 17:58:09 -0700

    improved JS to display one card at a time

--- public/js/game.js
+++ public/js/game.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
   var counter = 0;
-  $('#definition_' + counter).removeClass('hidden').addClass('active');
-  $('#answer_' + counter).removeClass('hidden').addClass('active');
+  var count = $('.hidden').length;
+  console.log(count);
+  $('#definition_' + counter).parent().removeClass('hidden').addClass('active');
   $('.hidden').hide();
-  // set class to active where id's have counter value
   $('#next_card_btn').on('click', function() {
-    counter += 1;
-    $('.active').removeClass('active').addClass('hidden');
-    $('#definition_' + counter).removeClass('hidden').addClass('active');
-    $('#answer_' + counter).removeClass('hidden').addClass('active');
-    $('.hidden').hide();
-    $('.active').show();
-    // set class to active where id's have counter value
+    if (count == count) {
+      //display end of deck message
+    }
+    else {
+      counter += 1;
+      $('.active').removeClass('active').addClass('hidden');
+      $('#definition_' + counter).parent().removeClass('hidden').addClass('active');
+      $('.hidden').hide();
+      $('.active').show();
+    }
   });
 });
