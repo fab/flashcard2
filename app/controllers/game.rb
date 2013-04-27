8dad01e8606953f2232eb5531440d4a1db0474c5
Fab Mackojc <fab.mackojc@gmail.com>
2013-04-26 22:11:19 -0700

    user can now play a game without feedback

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -2,3 +2,11 @@ get '/deck/:id/play' do
   @deck = Deck.find(params[:id])
   erb :game
 end
+
+post '/check_answer' do
+  p params
+  puts data = Card.find(params['card_id']).answer == params['answer'] 
+  data.to_s
+  #store card, session[:user_id] in round table
+  #return true/false or whatever
+end
--- public/js/game.js
+++ public/js/game.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-  var counter = 0;
-  var num_cards = $('.hidden').length;
-  $('#definition_' + counter).parent().removeClass('hidden').addClass('active');
-  $('.hidden').hide();
-  $('#next_card_btn').on('click', function() {
+  var nextCard = function() {
     counter += 1;
     if (counter == num_cards) {
       $('.active').hide();
-      alert('The deck is empty!')
+      alert('The deck is empty!');
       //display end of deck message
     }
     else {
@@ -16,5 +12,40 @@ $(document).ready(function() {
       $('.hidden').hide();
       $('.active').show();
     }
+  };
+  console.log('howdy1');
+  var counter = 0;
+  var num_cards = $('.hidden').length;
+  $('#definition_' + counter).parent().removeClass('hidden').addClass('active');
+  $('.hidden').hide();
+  // $('#next_card_btn').on('click', nextCard());
+
+  // Change colour of selected button
+
+  $('.answers').children().on('click', function (e){
+    console.log('howdy');
+    e.preventDefault();
+  var card_id = $('.active').find('input').val();
+  var answer = $(this).text();
+  $(this).addClass('clicked');
+  console.log(card_id);
+  console.log(answer);
+  console.log($(this));
+  $.ajax({
+    type: 'post',
+    url: '/check_answer',
+    data: "card_id=" + card_id + "&answer=" + answer
+  }).done(function(data){
+    console.log(data);
+    if(data == "true"){
+      console.log($(this));
+      $('.clicked').addClass('correct');
+    }else{
+      console.log($(this));
+      $('.clicked').addClass('wrong');
+    }
+    setTimeout(nextCard(), 3000);
+  });
   });
 });
+
