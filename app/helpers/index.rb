6dbf9db2bb68e3de5f1bb9c6aca5e9792b06baa3
Fab Mackojc <fab.mackojc@gmail.com>
2013-04-26 20:52:09 -0700

    Radomized answers and added card.id hidden field

--- app/helpers/index.rb
+++ app/helpers/index.rb
@@ -3,8 +3,14 @@ helpers do
     erb(template, :layout => false, :locals => locals)
   end
 
-  def three_wrong_answers(card)
-    three_cards = @deck.cards.where('id != ?', card.id).sample(3)
-    three_cards.map { |card| card.answer }
+  # def three_wrong_answers(card)
+  #   three_cards = @deck.cards.where('id != ?', card.id).sample(3)
+  #   three_cards.map { |card| card.answer }
+  # end
+
+  def multiple_choice_answers(card)
+    answers = @deck.cards.where('id != ?', card.id).sample(3)
+    answers << card
+    answers.shuffle
   end
 end
