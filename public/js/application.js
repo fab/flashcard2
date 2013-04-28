8b2d92934a3298cf06486078a522e3d324fad15f
Apprentice <apprentice@devbootcamp.com>
2013-04-27 20:20:53 -0700

    users can now click on a deck to play it.

--- public/js/application.js
+++ public/js/application.js
@@ -15,13 +15,13 @@ var hideInfo = function(){
 $(document).on('mouseover', '.deck_preview', function(){showInfo();});
 $(document).on('mouseout', '.deck_info', function(){hideInfo();});
 $(document).ready(function() {
+  $('.deck_holder').click(function(){
+    console.log($(this).serialize());
+    console.log('help me');
+  });
 
-  
-
-
-
-
-
-
-
+  $('.deck_info').click(function(){
+    var deck_id = $(this).find('.deck_id').val();
+    window.location = '/deck/'+ deck_id +'/play'
+  });
 });
